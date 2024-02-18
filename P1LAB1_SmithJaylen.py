6679f39cbf26c7c00f225053c9af754f3824a137
smithj1008 <160432474+smithj1008@users.noreply.github.com>
2024-02-18 16:45:45 -0500

    Add files via upload
    First Submission

---
+++ P1LAB1_SmithJaylen.py
@@ -0,0 +1,5 @@
+# Each print statement starts on a new line
+print("Jaylen")
+print("2/18/2024")
+print("P1LAB1")
+print("This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.")
